e7ce3b486f06826555ada243e9d582e12d0dcb0f
Valerie <valpasq@bu.edu>
2015-10-13 17:38:14 -0400

    renamed remove script, changed raw and sr remove functions for use with new USGS file names

--- clean_remove_files.sh
+++ clean_remove_files.sh
@@ -11,16 +11,18 @@ here=$1
 cd $here
 
 # Remove raw band TIFFs
-find ./ -name '*band*.tif' -exec rm -v {} \;
+#find ./ -name 'L*band*.tif' -exec rm -v {} \;
+find ./ -regextype posix-egrep -regex 'L.*[^sr]_band.*.tif' -exec rm -v {} \;
 
 # Remove LEDAPS and indicies TIFFs
-find ./ -name 'L*sr*.tif' -exec rm -v {} \;
+#find ./ -name 'L*sr*.tif' -exec rm -v {} \;
+find ./ -regextype posix-egrep -regex 'L.*sr_band.*.tif' -exec rm -v {} \;
 
 # Remove brightness temperature TIFFs
-find ./ -name 'L*toa*.tif' -exec rm -v {} \;
+#find ./ -name 'L*toa*.tif' -exec rm -v {} \;
 
 # Remove fmask TIFFs
-find ./ -name 'L*cfmask.tif' -exec rm -v {} \;
+#find ./ -name 'L*cfmask.tif' -exec rm -v {} \;
 
 # Remove stats images (not useful)
 #find ./ -name '*.png' -exec rm -v {} \;
@@ -33,6 +35,9 @@ find ./ -name 'L*cfmask.tif' -exec rm -v {} \;
 
 # Remove stacks
 #find ./ -name '*_stack*' -exec rm -v {} \;
+
+# Remove BGW
+find ./ -name '*_BGW.tif*' -exec rm -v {} \;
 
 
 echo "Done!"
