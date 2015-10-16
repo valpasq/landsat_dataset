05092ea78208de2090577cf61d9cf37937290486
Valerie <valpasq@bu.edu>
2015-10-16 09:23:37 -0400

    generalized inputs =dir =string path/row, eg p012r031

--- 0_gen_espa_list.sh
+++ 0_gen_espa_list.sh
@@ -1,12 +1,22 @@
+#!/bin/bash
 
-cd /projectnb/landsat/projects/Massachusetts/p012r031/
+if [ -z "$1" ]; then
+    echo "Error - please specify a directory with extraced Landsat archives. Usage:"
+    echo "    $0 <directory>"
+    exit 1
+fi
 
-output=./p012r031_submit.txt
+here=$1
+WRS=$2
 
-cat ./LSR_LANDSAT_ETM_COMBINED_p012r031.txt | awk -F ',' 'NR > 1 { print $2 }' > $output
-cat ./LSR_LANDSAT_TM_p012r031.txt | awk -F ',' 'NR > 1 { print $2 }' >> $output
+cd $here
+
+output=./${WRS}_submit.txt
+
+cat ./LSR_LANDSAT_ETM_COMBINED_${WRS}.txt | awk -F ',' 'NR > 1 { print $2 }' > $output
+cat ./LSR_LANDSAT_TM_${WRS}.txt | awk -F ',' 'NR > 1 { print $2 }' >> $output
 # cat ./LANDSAT_8_28473.txt | awk -F ',' 'NR > 1 { print $2 }' > $output
 
-n=$(cat ./p012r031_submit.txt | wc -l)
+n=$(cat ./${WRS}_submit.txt | wc -l)
 
 echo "You have $n Landsat images in your text file"
