168dbccaca569c2e1c351f8d786edc23cb8cef0f
Valerie <valpasq@bu.edu>
2015-11-08 14:48:00 -0500

    updated to work with cli yatsm

--- yatsm_map_amplitude.sh
+++ yatsm_map_amplitude.sh
@@ -1,45 +1,52 @@
 #!/bin/bash
+#$ -l h_rt=72:00:00
+#$ -V
+#$ -N map_amplitudes
+#$ -j y
 
 source ~/modules.sh
 
-date=2005-04-01
+cd $1
+
+date=2006-06-01
+
 
 # Band 1
-~/Documents/yatsm/scripts/yatsm_map.py -v --band "1" \
---ndv -9999 --before --amplitude coef $date ~/harm_b1.gtif
+yatsm map --band 1 \
+--ndv -9999 --before --amplitude coef $date ./harm_b1.gtif
 
 # Band 2
-~/Documents/yatsm/scripts/yatsm_map.py -v --band "2" \
---ndv -9999 --before --amplitude coef $date ~/harm_b2.gtif
+yatsm map --band 2 \
+--ndv -9999 --before --amplitude coef $date ./harm_b2.gtif
 
 # Band 3
-~/Documents/yatsm/scripts/yatsm_map.py -v --band "3" \
---ndv -9999 --before --amplitude coef $date ~/harm_b3.gtif
+yatsm map --band 3 \
+--ndv -9999 --before --amplitude coef $date ./harm_b3.gtif
 
 # Band 4
-~/Documents/yatsm/scripts/yatsm_map.py -v --band "4" \
---ndv -9999 --before --amplitude coef $date ~/harm_b4.gtif
+yatsm map --band 4 \
+--ndv -9999 --before --amplitude coef $date ./harm_b4.gtif
 
 # Band 5
-~/Documents/yatsm/scripts/yatsm_map.py -v --band "5" \
---ndv -9999 --before --amplitude coef $date ~/harm_b5.gtif
+yatsm map --band 5 \
+--ndv -9999 --before --amplitude coef $date ./harm_b5.gtif
 
 # Band 6
-~/Documents/yatsm/scripts/yatsm_map.py -v --band "6" \
---ndv -9999 --before --amplitude coef $date ~/harm_b6.gtif
+yatsm map --band 6 \
+--ndv -9999 --before --amplitude coef $date ./harm_b6.gtif
 
 # Band 7
-~/Documents/yatsm/scripts/yatsm_map.py -v --band "7" \
---ndv -9999 --before --amplitude coef $date ~/harm_b7.gtif
+yatsm map --band 7 \
+--ndv -9999 --before --amplitude coef $date ./harm_b7.gtif
 
 # Band 8
-~/Documents/yatsm/scripts/yatsm_map.py -v --band "8" \
---ndv -9999 --before --amplitude coef $date ~/harm_B.gtif
+yatsm map --band 8 \
+--ndv -9999 --before --amplitude coef $date ./harm_B.gtif
 
 # Band 9
-~/Documents/yatsm/scripts/yatsm_map.py -v --band "9" \
---ndv -9999 --before --amplitude coef $date ~/harm_G.gtif
+yatsm map --band 9 \
+--ndv -9999 --before --amplitude coef $date ./harm_G.gtif
 
 # Band 10
-~/Documents/yatsm/scripts/yatsm_map.py -v --band "10" \
---ndv -9999 --before --amplitude coef $date ~/harm_W.gtif
+yatsm map --band 10 \
+--ndv -9999 --before --amplitude coef $date ./harm_W.gtif
