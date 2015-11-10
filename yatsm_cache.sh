184172515242be7e6c634a56fef27c877f29d431
Valerie <valpasq@bu.edu>
2015-11-10 17:58:22 -0500

    changed job name to cache_

--- yatsm_cache.sh
+++ yatsm_cache.sh
@@ -10,7 +10,7 @@ module load R/R-3.1.1
 
 njob=50
 for job in $(seq 1 $njob); do 
-    qsub -j y -V -l h_rt=72:00:00 -N yatsm_$job -l eth_speed=10 -b y  \
+    qsub -j y -V -l h_rt=72:00:00 -N cache_$job -l eth_speed=10 -b y  \
         yatsm -v cache \
         /projectnb/landsat/users/valpasq/Southwest_fires/p035r032_config.yaml \
         $job $njob
