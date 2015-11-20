5b8ce1fc6b76945ef93f368b990777271da92930
Valerie <valpasq@bu.edu>
2015-11-20 12:21:58 -0500

    generalized such that stack name pattern is second input - need to test

--- create_example_img.sh
+++ create_example_img.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "Error - please specify a directory with extraced Landsat archives. Usage:"
+    echo "    $0 <directory>"
+    exit 1
+fi
+
+# First input is location of image directory
+here=$1
+cd $here
+
+# Second input is file pattern for example images
+pattern=$2
+
 # find dir/name of first stack
-example=$(find . -name 'L*all' -type f | head -1)
+example=$(find . -name $pattern -type f | head -1)
 echo $example
 
 # create example image using gdal translate
