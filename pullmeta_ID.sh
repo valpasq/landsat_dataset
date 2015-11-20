8871d1fb8c72a8aecda7893d7972970f34b21ec7
Valerie <valpasq@bu.edu>
2015-11-20 12:14:23 -0500

    added script for pulling ID, date and ACCA score from metadata

---
+++ pullmeta_ID.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Error - please specify a directory with extraced Landsat archives. Usage:"
+    echo "    $0 <directory>"
+    exit 1
+fi
+
+here=$1
+
+cd $here
+
+output=./p011r031_clear_80.txt
+
+
+# ID AND DATE - FOR SYMBOLOGY
+#echo "ID, DATE" > $output
+#for mtl in $(find ./ -name '*MTL.txt'); do
+#    id=$(grep -a -e "LANDSAT_SCENE_ID" $mtl | tr -d ' ' | awk -F '=' '{ print $2 }') 
+#    date=$(grep -a -e "DATE_ACQUIRED" $mtl | tr -d ' ' | awk -F '=' '{ print $2 }')
+#    echo "$id, $date" >> $output
+#done        
+
+# CLOUD COVER
+echo "ID, DATE, ACCA" > $output
+for mtl in $(find ./ -name '*MTL.txt'); do
+    id=$(grep -a -e "LANDSAT_SCENE_ID" $mtl | tr -d ' ' | awk -F '=' '{ print $2 }') 
+    date=$(grep -a -e "DATE_ACQUIRED" $mtl | tr -d ' ' | awk -F '=' '{ print $2 }')
+    ACCA=$(grep -a -e "CLOUD_COVER" $mtl | tr -d ' ' | awk -F '=' '{ print $2 }')
+    echo "$id, $date, $ACCA" >> $output
+done     
+
+
+#for img in $(find ./ -name '*stack'); do
+#    dir=$(pwd)/$(basename "$img")
+#    echo "$dir" >> $output    
+#done
+    
+
+
+
+    
