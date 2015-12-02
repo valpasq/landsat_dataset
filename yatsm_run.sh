8fe5ea1f819e95fb2fe7c9ef311496e73edc9ffb
Valerie <valpasq@bu.edu>
2015-12-02 08:40:34 -0500

    removed module loading

--- yatsm_run.sh
+++ yatsm_run.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-module load python/2.7.5
-module load gdal/1.11.1
-module load R/R-3.1.1
+config=$1
 
-njob=25
+njob=100
 for job in $(seq 1 $njob); do 
-    qsub -j y -V -l h_rt=24:00:00 -N yatsm_$job -l eth_speed=10 -b y  \
-        yatsm line /projectnb/landsat/projects/IDS/p012r031/p012r031_config.yaml \
+    qsub -j y -V -l h_rt=72:00:00 -N yatsm_$job -l eth_speed=10 -b y  \
+        yatsm -v line --resume $config \
         $job $njob
 done
