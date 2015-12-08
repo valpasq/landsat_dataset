2caee99702174e85431e63594b5d0b555c5a171b
Valerie <valpasq@bu.edu>
2015-12-08 10:04:38 -0500

    removed etherspeed input, shortened run time, increased num of jobs

--- yatsm_run.sh
+++ yatsm_run.sh
@@ -2,9 +2,9 @@
 
 config=$1
 
-njob=100
+njob=500
 for job in $(seq 1 $njob); do 
-    qsub -j y -V -l h_rt=72:00:00 -N yatsm_$job -l eth_speed=10 -b y  \
+    qsub -j y -V -l h_rt=24:00:00 -N yatsm_$job -b y  \
         yatsm -v line --resume $config \
         $job $njob
 done
