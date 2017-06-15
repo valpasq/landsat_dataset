d9874e69204a22742f7fa4c19c3a2d80fdd26310
Valerie <valpasq@bu.edu>
2017-06-15 11:35:46 -0400

    updated metadata script to include sun azimuth and elevation

--- pullmeta_ID.sh
+++ pullmeta_ID.sh
@@ -13,31 +13,11 @@ cd $here
 # Second input is output file name, e.g. output=./p011r031_clear_80.txt
 output=$2
 
-
-# ID AND DATE - FOR SYMBOLOGY
-#echo "ID, DATE" > $output
-#for mtl in $(find ./ -name '*MTL.txt'); do
-#    id=$(grep -a -e "LANDSAT_SCENE_ID" $mtl | tr -d ' ' | awk -F '=' '{ print $2 }') 
-#    date=$(grep -a -e "DATE_ACQUIRED" $mtl | tr -d ' ' | awk -F '=' '{ print $2 }')
-#    echo "$id, $date" >> $output
-#done        
-
-# CLOUD COVER
-echo "ID, DATE, ACCA" > $output
+echo "image_ID,standard_date,azimuth,elevation" > $output
 for mtl in $(find ./ -name '*MTL.txt'); do
     id=$(grep -a -e "LANDSAT_SCENE_ID" $mtl | tr -d ' ' | awk -F '=' '{ print $2 }') 
     date=$(grep -a -e "DATE_ACQUIRED" $mtl | tr -d ' ' | awk -F '=' '{ print $2 }')
-    ACCA=$(grep -a -e "CLOUD_COVER" $mtl | tr -d ' ' | awk -F '=' '{ print $2 }')
-    echo "$id, $date, $ACCA" >> $output
+    AZ=$(grep -a -e "SUN_AZIMUTH" $mtl | tr -d ' ' | awk -F '=' '{ print $2 }')
+    ELEV=$(grep -a -e "SUN_ELEVATION" $mtl | tr -d ' ' | awk -F '=' '{ print $2 }')
+    echo "$id, $date, $AZ, $ELEV" >> $output
 done     
-
-
-#for img in $(find ./ -name '*stack'); do
-#    dir=$(pwd)/$(basename "$img")
-#    echo "$dir" >> $output    
-#done
-    
-
-
-
-    
